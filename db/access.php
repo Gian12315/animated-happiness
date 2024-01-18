f77d7806eb40416bee528fab5373a384e31a5045
Gian12315 <giancarlo.cytro@gmail.com>
2024-01-17 21:49:25 -0600

    Several removals and changes

--- db/access.php
+++ db/access.php
@@ -1,7 +1,7 @@
 <?php
     $capabilities = array(
 
-    'block/simplecamera:myaddinstance' => array(
+    'block/sentimentsdata:myaddinstance' => array(
         'captype' => 'write',
         'contextlevel' => CONTEXT_SYSTEM,
         'archetypes' => array(
@@ -11,7 +11,7 @@
         'clonepermissionsfrom' => 'moodle/my:manageblocks'
     ),
 
-    'block/simplecamera:addinstance' => array(
+    'block/sentimentsdata:addinstance' => array(
         'riskbitmask' => RISK_SPAM | RISK_XSS,
 
         'captype' => 'write',
--- edit_form.php
+++ edit_form.php
@@ -1,6 +1,6 @@
 <?php
 
-class block_simplecamera_edit_form extends block_edit_form {
+class block_sentimentsdata_edit_form extends block_edit_form {
         
     protected function specific_definition($mform) {
         
@@ -8,7 +8,7 @@ class block_simplecamera_edit_form extends block_edit_form {
         $mform->addElement('header', 'config_header', get_string('blocksettings', 'block'));
 
         // A sample string variable with a default value.
-        $mform->addElement('text', 'config_period', get_string('cameraperiod', 'block_simplecamera'));
+        $mform->addElement('text', 'config_period', get_string('cameraperiod', 'block_sentimentsdata'));
         $mform->setDefault('config_period', 10);
         $mform->setType('config_period', PARAM_INT);        
 
